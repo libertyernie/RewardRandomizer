1f7472c18d6ce9dc0c0a788053ad8b49996bb783
libertyernie <maybeway36@gmail.com>
2022-07-21 19:01:53 -0500

    tweak GUI

--- RewardRandomizer.VBGUI/Form1.Designer.vb
+++ RewardRandomizer.VBGUI/Form1.Designer.vb
@@ -30,10 +30,8 @@ Partial Class Form1
         Me.RandomizeStatBoosters = New System.Windows.Forms.CheckBox()
         Me.RandomizePromotionItems = New System.Windows.Forms.CheckBox()
         Me.ShuffleStatBoosters = New System.Windows.Forms.CheckBox()
-        Me.Label4 = New System.Windows.Forms.Label()
         Me.ShufflePromoItems = New System.Windows.Forms.CheckBox()
         Me.ShuffleVillages = New System.Windows.Forms.CheckBox()
-        Me.Label3 = New System.Windows.Forms.Label()
         Me.ShuffleChests = New System.Windows.Forms.CheckBox()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
@@ -41,6 +39,10 @@ Partial Class Form1
         Me.ShuffleDesert = New System.Windows.Forms.CheckBox()
         Me.ValidateButton = New System.Windows.Forms.Button()
         Me.ExcludeConsumables = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
@@ -59,13 +61,13 @@ Partial Class Form1
         Me.InputBox.Location = New System.Drawing.Point(83, 12)
         Me.InputBox.Name = "InputBox"
         Me.InputBox.ReadOnly = True
-        Me.InputBox.Size = New System.Drawing.Size(208, 20)
+        Me.InputBox.Size = New System.Drawing.Size(158, 20)
         Me.InputBox.TabIndex = 1
         '
         'BrowseInputButton
         '
         Me.BrowseInputButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.BrowseInputButton.Location = New System.Drawing.Point(297, 12)
+        Me.BrowseInputButton.Location = New System.Drawing.Point(247, 11)
         Me.BrowseInputButton.Name = "BrowseInputButton"
         Me.BrowseInputButton.Size = New System.Drawing.Size(75, 20)
         Me.BrowseInputButton.TabIndex = 2
@@ -83,8 +85,8 @@ Partial Class Form1
         '
         'SaveButton
         '
-        Me.SaveButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.SaveButton.Location = New System.Drawing.Point(297, 200)
+        Me.SaveButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SaveButton.Location = New System.Drawing.Point(247, 279)
         Me.SaveButton.Name = "SaveButton"
         Me.SaveButton.Size = New System.Drawing.Size(75, 23)
         Me.SaveButton.TabIndex = 17
@@ -94,7 +96,7 @@ Partial Class Form1
         'RandomizeStatBoosters
         '
         Me.RandomizeStatBoosters.AutoSize = True
-        Me.RandomizeStatBoosters.Location = New System.Drawing.Point(198, 172)
+        Me.RandomizeStatBoosters.Location = New System.Drawing.Point(6, 88)
         Me.RandomizeStatBoosters.Name = "RandomizeStatBoosters"
         Me.RandomizeStatBoosters.Size = New System.Drawing.Size(142, 17)
         Me.RandomizeStatBoosters.TabIndex = 15
@@ -104,7 +106,7 @@ Partial Class Form1
         'RandomizePromotionItems
         '
         Me.RandomizePromotionItems.AutoSize = True
-        Me.RandomizePromotionItems.Location = New System.Drawing.Point(15, 172)
+        Me.RandomizePromotionItems.Location = New System.Drawing.Point(6, 65)
         Me.RandomizePromotionItems.Name = "RandomizePromotionItems"
         Me.RandomizePromotionItems.Size = New System.Drawing.Size(155, 17)
         Me.RandomizePromotionItems.TabIndex = 13
@@ -116,29 +118,19 @@ Partial Class Form1
         Me.ShuffleStatBoosters.AutoSize = True
         Me.ShuffleStatBoosters.Checked = True
         Me.ShuffleStatBoosters.CheckState = System.Windows.Forms.CheckState.Checked
-        Me.ShuffleStatBoosters.Location = New System.Drawing.Point(198, 149)
+        Me.ShuffleStatBoosters.Location = New System.Drawing.Point(6, 42)
         Me.ShuffleStatBoosters.Name = "ShuffleStatBoosters"
         Me.ShuffleStatBoosters.Size = New System.Drawing.Size(122, 17)
         Me.ShuffleStatBoosters.TabIndex = 14
         Me.ShuffleStatBoosters.Text = "Shuffle stat boosters"
         Me.ShuffleStatBoosters.UseVisualStyleBackColor = True
         '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(12, 130)
-        Me.Label4.Margin = New System.Windows.Forms.Padding(3)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(45, 13)
-        Me.Label4.TabIndex = 11
-        Me.Label4.Text = "But first:"
-        '
         'ShufflePromoItems
         '
         Me.ShufflePromoItems.AutoSize = True
         Me.ShufflePromoItems.Checked = True
         Me.ShufflePromoItems.CheckState = System.Windows.Forms.CheckState.Checked
-        Me.ShufflePromoItems.Location = New System.Drawing.Point(15, 149)
+        Me.ShufflePromoItems.Location = New System.Drawing.Point(6, 19)
         Me.ShufflePromoItems.Name = "ShufflePromoItems"
         Me.ShufflePromoItems.Size = New System.Drawing.Size(135, 17)
         Me.ShufflePromoItems.TabIndex = 12
@@ -150,29 +142,19 @@ Partial Class Form1
         Me.ShuffleVillages.AutoSize = True
         Me.ShuffleVillages.Checked = True
         Me.ShuffleVillages.CheckState = System.Windows.Forms.CheckState.Checked
-        Me.ShuffleVillages.Location = New System.Drawing.Point(79, 84)
+        Me.ShuffleVillages.Location = New System.Drawing.Point(70, 19)
         Me.ShuffleVillages.Name = "ShuffleVillages"
         Me.ShuffleVillages.Size = New System.Drawing.Size(62, 17)
         Me.ShuffleVillages.TabIndex = 8
         Me.ShuffleVillages.Text = "Villages"
         Me.ShuffleVillages.UseVisualStyleBackColor = True
         '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(12, 65)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(3)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(156, 13)
-        Me.Label3.TabIndex = 6
-        Me.Label3.Text = "Shuffle all items that come from:"
-        '
         'ShuffleChests
         '
         Me.ShuffleChests.AutoSize = True
         Me.ShuffleChests.Checked = True
         Me.ShuffleChests.CheckState = System.Windows.Forms.CheckState.Checked
-        Me.ShuffleChests.Location = New System.Drawing.Point(15, 84)
+        Me.ShuffleChests.Location = New System.Drawing.Point(6, 19)
         Me.ShuffleChests.Name = "ShuffleChests"
         Me.ShuffleChests.Size = New System.Drawing.Size(58, 17)
         Me.ShuffleChests.TabIndex = 7
@@ -183,7 +165,7 @@ Partial Class Form1
         '
         Me.Label6.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(12, 226)
+        Me.Label6.Location = New System.Drawing.Point(12, 326)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(69, 13)
         Me.Label6.TabIndex = 18
@@ -194,7 +176,7 @@ Partial Class Form1
         Me.Label7.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.Label7.AutoSize = True
         Me.Label7.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Label7.Location = New System.Drawing.Point(12, 239)
+        Me.Label7.Location = New System.Drawing.Point(12, 339)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(248, 13)
         Me.Label7.TabIndex = 19
@@ -209,7 +191,7 @@ Partial Class Form1
         Me.ComboBox1.FormattingEnabled = True
         Me.ComboBox1.Location = New System.Drawing.Point(83, 38)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(208, 21)
+        Me.ComboBox1.Size = New System.Drawing.Size(158, 21)
         Me.ComboBox1.TabIndex = 4
         '
         'ShuffleDesert
@@ -217,7 +199,7 @@ Partial Class Form1
         Me.ShuffleDesert.AutoSize = True
         Me.ShuffleDesert.Checked = True
         Me.ShuffleDesert.CheckState = System.Windows.Forms.CheckState.Checked
-        Me.ShuffleDesert.Location = New System.Drawing.Point(147, 84)
+        Me.ShuffleDesert.Location = New System.Drawing.Point(138, 19)
         Me.ShuffleDesert.Name = "ShuffleDesert"
         Me.ShuffleDesert.Size = New System.Drawing.Size(57, 17)
         Me.ShuffleDesert.TabIndex = 9
@@ -227,7 +209,7 @@ Partial Class Form1
         'ValidateButton
         '
         Me.ValidateButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.ValidateButton.Location = New System.Drawing.Point(297, 38)
+        Me.ValidateButton.Location = New System.Drawing.Point(247, 37)
         Me.ValidateButton.Name = "ValidateButton"
         Me.ValidateButton.Size = New System.Drawing.Size(75, 21)
         Me.ValidateButton.TabIndex = 5
@@ -239,39 +221,65 @@ Partial Class Form1
         Me.ExcludeConsumables.AutoSize = True
         Me.ExcludeConsumables.Checked = True
         Me.ExcludeConsumables.CheckState = System.Windows.Forms.CheckState.Checked
-        Me.ExcludeConsumables.Location = New System.Drawing.Point(15, 107)
+        Me.ExcludeConsumables.Location = New System.Drawing.Point(6, 42)
         Me.ExcludeConsumables.Name = "ExcludeConsumables"
         Me.ExcludeConsumables.Size = New System.Drawing.Size(276, 17)
         Me.ExcludeConsumables.TabIndex = 10
         Me.ExcludeConsumables.Text = "Exclude vulneraries, elixirs, pure water, and antitoxins"
         Me.ExcludeConsumables.UseVisualStyleBackColor = True
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.RandomizeStatBoosters)
+        Me.GroupBox1.Controls.Add(Me.RandomizePromotionItems)
+        Me.GroupBox1.Controls.Add(Me.ShuffleStatBoosters)
+        Me.GroupBox1.Controls.Add(Me.ShufflePromoItems)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 65)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(310, 124)
+        Me.GroupBox1.TabIndex = 20
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "By category"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox2.Controls.Add(Me.ExcludeConsumables)
+        Me.GroupBox2.Controls.Add(Me.ShuffleDesert)
+        Me.GroupBox2.Controls.Add(Me.ShuffleVillages)
+        Me.GroupBox2.Controls.Add(Me.ShuffleChests)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 195)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(310, 78)
+        Me.GroupBox2.TabIndex = 21
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Shuffle all"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(384, 261)
-        Me.Controls.Add(Me.ExcludeConsumables)
+        Me.ClientSize = New System.Drawing.Size(334, 361)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.ValidateButton)
-        Me.Controls.Add(Me.ShuffleDesert)
         Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.SaveButton)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.ShuffleChests)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.ShuffleVillages)
-        Me.Controls.Add(Me.RandomizeStatBoosters)
         Me.Controls.Add(Me.BrowseInputButton)
-        Me.Controls.Add(Me.ShufflePromoItems)
-        Me.Controls.Add(Me.RandomizePromotionItems)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.ShuffleStatBoosters)
         Me.Controls.Add(Me.InputBox)
         Me.Name = "Form1"
         Me.Text = "Reward Randomizer"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -283,9 +291,7 @@ Partial Class Form1
     Friend WithEvents Label2 As Label
     Friend WithEvents ShuffleVillages As CheckBox
     Friend WithEvents ShuffleChests As CheckBox
-    Friend WithEvents Label3 As Label
     Friend WithEvents ShuffleStatBoosters As CheckBox
-    Friend WithEvents Label4 As Label
     Friend WithEvents ShufflePromoItems As CheckBox
     Friend WithEvents RandomizeStatBoosters As CheckBox
     Friend WithEvents RandomizePromotionItems As CheckBox
@@ -296,4 +302,6 @@ Partial Class Form1
     Friend WithEvents ShuffleDesert As CheckBox
     Friend WithEvents ValidateButton As Button
     Friend WithEvents ExcludeConsumables As CheckBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
 End Class
